37d04c00bcb09c0af4c775f2fd0f662d59e2a661
samu-koi312 <167263929+samu-koi312@users.noreply.github.com>
2024-04-17 10:13:51 +0900

    java課題015をアップロード

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,31 @@
+package kadai_015;
+
+//車クラス
+public class Car_Chapter15 {
+	
+	private int gear;
+	private int speed;
+	
+	public Car_Chapter15(int gear, int speed) {
+		this.gear = gear;
+		this.speed = speed;
+	}
+	
+	public void gearChange(int afterGear) {
+		System.out.println("ギア"+this.gear+"から"+afterGear+"に切り替えました");
+		this.gear = afterGear;
+	}
+	
+	public void run() {
+		this.speed = switch(this.gear) {
+			case 1 -> 10;
+			case 2 -> 20;
+			case 3 -> 30;
+			case 4 -> 40;
+			case 5 -> 50;
+			default -> 10;
+		};
+		System.out.println("速度は時速"+this.speed+"です");
+	}
+
+}
